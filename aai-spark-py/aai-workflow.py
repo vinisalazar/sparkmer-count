da31142f956d018d55b8ff6e4cb24a1b4304389c
thiago.souza <thiago.souza>
2019-11-30 21:21:53 -0300

    problema com o java landa nahora de gerar a lista de tuplas

--- aai-spark-py/aai-workflow.py
+++ aai-spark-py/aai-workflow.py
@@ -4,35 +4,58 @@ from pyspark.context import SparkConf
 from pyspark.sql import Row
 from pyspark.sql.window import Window
 from pyspark.sql import functions as F
-from pyspark.sql.functions import desc, asc
+from pyspark.sql.functions import udf
+from pyspark.sql.functions import col
+from operator import add
+from functools import reduce
 from bio_spark.io.fasta_reader import FASTAReade
+import collections
+import numpy as np
 
-def seq2kmer(id_, seq_, k):
-    num_kmers = len(seq_) - k + 1
-    kmers_list = [(id_, seq_[n*k:k*(n+1)]) for n in range(0, num_kmers)]
+def seq2kmer(seq_):
+    value = seq_[0].strip()    
+    k = 3
+    num_kmers = len(value) - k + 1
+    kmers_list = [value[n*k:k*(n+1)] for n in range(0, num_kmers)]
+    
+    # return len(value)
     return kmers_list
+
+seq2kmer_udf = udf(seq2kmer)
+
+def kmers_list2kmers_freq_dict(kmers_list):
+    kmers_list_np = np.array(kmers_list)
+    flatten_data = kmers_list_np.flatten()
+    unique, counts = np.unique(flatten_data, return_counts=True)
+    kmers_list = list(zip(unique.tolist(), counts.tolist()))
+    return kmers_list
+
+kmers_list2kmers_freq_dict_udf = udf(kmers_list2kmers_freq_dict)
 
 if __name__ == "__main__":
     sConf = SparkConf("spark://localhost:7077")
     sc = SparkContext(conf=sConf)
     spark = SparkSession(sc)
 
-    plain_df = sc.textFile("../data/SP1.fq").map(lambda x: Row(row=x)).toDF()
+    plain_df = sc.textFile("/home/thiago/Dados/sparkAAI-1/data/SP1.fq").map(lambda x: Row(row=x)).toDF()
  
     reader = FASTAReade(sc)
     parsedDF = reader.read(plain_df)
     parsedDF.coalesce(4)
 
-    grouped_by_seq_rdd = parsedDF.groupby("seqID")
+    kmers_of_seqs_df = parsedDF\
+            .withColumn("kmers", seq2kmer_udf("seq"))\
 
-    ## finding kmers
-    print(type(parsedDF.rdd))
-    kmers_rdd = parsedDF.select("seqID", "seq").rdd\
-                .flatMap(lambda c: seq2kmer(c.seqID[0], c.seq[0],3))                
+    print(kmers_of_seqs_df.show())
 
-    print(kmers_rdd.toDF().show())  
-    reduced_rdd = kmers_rdd.reduce(lambda   )
+    grouped_by_seq_df = kmers_of_seqs_df\
+            .groupby("seqID")\
+            .agg(F.collect_list('kmers').alias('kmers_list'))\
+            .withColumn('kmers_freq', kmers_list2kmers_freq_dict_udf('kmers_list'))
+            # .count()
+            # .agg(F.count)
 
-    print(reduced_rdd.toDF().show())  
-    # print(kmers_rdd.take(5))
-
+    result = grouped_by_seq_df.select("kmers_freq").take(2)
+    print(result[0][0])
+    # print(grouped_by_seq_df.show())
+    
